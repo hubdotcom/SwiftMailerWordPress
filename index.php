c70c38bb9a0d3b753b3acc51be275bdeeb3fb2fa
Turn On Social <Turn On Social@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-01-15 04:50:24 +0000

    v4.3.0
    git-svn-id: https://plugins.svn.wordpress.org/swift-mailer/trunk@652778 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ index.php
@@ -0,0 +1,11 @@
+<?
+/*
+Plugin Name: Swift Mailer
+Plugin URI: turnonsocial.com
+Description: The Swift Mailer library and nothing more
+Version: 4.3.0
+*/
+
+require("Swift-4.3.0/lib/swift_required.php");
+
+?>
